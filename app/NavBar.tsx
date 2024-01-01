b9f03c1607bdaeb440af2ded82c61be2bf44f366
shahayush480 <shahayush480@gmail.com>
2024-01-01 14:58:16 +0530

    styles active

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,4 +1,6 @@
+"use client";
 import Link from 'next/link'
+import { usePathname } from 'next/navigation';
 import React from 'react'
 import { FaBug } from "react-icons/fa6";
 const Links = [
@@ -6,12 +8,13 @@ const Links = [
     {label:"Issues" , href:"/issues"},
 ]
 const NavBar = () => {
+    const currentPath = usePathname()
   return (
     <nav className='flex items-center space-x-6 border-b mb-5 py-4 px-3 '>
         <Link href="/"><FaBug /></Link>
         <ul className='flex'>
             {Links.map((link,i)=>{
-                return <li key={i} className='px-3 opacity-70 hover:opacity-100 transition-opacity'><Link href="/">{link.label}</Link></li>
+                return <li key={i} className={`px-3 hover:opacity-100 transition-opacity ${currentPath == link.href ? "opacity-100" : "opacity-100" }`}><Link href="/">{link.label}</Link></li>
             })}
         </ul>
     </nav>
